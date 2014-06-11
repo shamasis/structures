50cd2aa360a0fa9a9d974bd34868200c88a467b5
Shamasis Bhattacharya <shamasis@fusioncharts.com>
2014-06-11 23:04:46 +0530

    Added new feature to limit the capacity of a stack. Relevant test cases were added too. Also completed the descriptions of API docs.

--- src/stack.js
+++ src/stack.js
@@ -2,13 +2,23 @@ var lib = require('../src/lib.js'),
     Stack;
 
 /**
+ * Stack is a particular kind of abstract data type or collection in which the principal (or only) operations on the
+ * collection are the addition of an entity to the collection, known as push and removal of an entity, known as pop.
+ * The relation between the push and pop operations is such that the stack is a Last-In-First-Out (LIFO) data structure.
+ * In a LIFO data structure, the last element added to the structure must be the first one to be removed. This is
+ * equivalent to the requirement that, considered as a linear data structure, or more abstractly a sequential
+ * collection, the push and pop operations occur only at one end of the structure, referred to as the top of the stack.
+ *
+ * _Citing: http://en.wikipedia.org/wiki/Stack_(abstract_data_type)_
+ *
  * @constructor
  */
 Stack = function () {
     this.length = 0;
+    this.limit = Infinity;
 };
 
-Stack.Item = function (value, prev) {
+Stack.StackItem = function (value, prev) {
     this.value = value;
     this.prev = prev;
 };
@@ -16,14 +26,25 @@ Stack.Item = function (value, prev) {
 lib.copy(Stack.prototype, /** @lends Stack.prototype */ {
 
     /**
-     * @param {*} value
-     * @returns {*}
+     * The `push` function is used to push a variable to the top of the stack. The variable pushed can be later
+     * retrieved by popping items from the stack using {@link Stack#pop}.
+     *
+     * @param {*} value The value that needs to be added to the top of the stack is passed as this parameter.
+     *
+     * @returns {*} The `push` function returns the same value that has been passed to the `value` parameter. This
+     * allows easy chaining and other coding styles to work seamlessly. In case stack limit has been set and stack is
+     * overflowing, the function returns `undefined`.
+     *
+     * @see Stack#pop
      */
     push: function (value) {
-        return (this.length++, (this.top = new Stack.Item(value, this.top)), value);
+        return (this.length < this.limit) ? (this.length++, (this.top = new Stack.StackItem(value, this.top)), value) :
+            undefined;
     },
 
     /**
+     * The `peek` function allows you to "peek" the top-most item in stack. In other words, it allows you to retrieve
+     * the item last pushed to the stack without actually popping (removing) it.
      * @returns {*}
      */
     peek: function () {
@@ -31,6 +52,7 @@ lib.copy(Stack.prototype, /** @lends Stack.prototype */ {
     },
 
     /**
+     * The `pop` function removes returns the topmost item in the stack (the item that was last "pushed").
      * @returns {*}
      */
     pop: function () {
@@ -39,6 +61,9 @@ lib.copy(Stack.prototype, /** @lends Stack.prototype */ {
     },
 
     /**
+     * Using the `size` function one can check the number of items that are present in the stack. `0` would indicate
+     * that the stack is empty.
+     *
      * @returns {number}
      */
     size: function () {
@@ -46,7 +71,29 @@ lib.copy(Stack.prototype, /** @lends Stack.prototype */ {
     },
 
     /**
+     * One can set a limit to the number of items that can be pushed to the stack by setting a value for the capacity.
+     * To revert to unlimited capacity, simply pass `Infinity` as the parameter. To retrieve the current capacity of the
+     * stack, call this function without any parameter.
      *
+     * @param {number=} [value] This parameter accepts a positive number to set as the capacity of the stack. If the
+     * number passed is less than the size of the stack, the overflowing items are popped.
+     * @returns {number}
+     */
+    capacity: function (value) {
+        // If parameter is a number and is greater than zero, we set it as new limit.
+        if (!isNaN(value) && value >= 0) {
+            this.limit = +value;
+            // In case current size is greater than the limit, we pop the items till size is met.
+            while (this.length > this.limit) {
+                this.pop();
+            }
+        }
+
+        return this.limit;
+    },
+
+    /**
+     * The entire stack can be cleared using this function without popping it one at a time.
      */
     clear: function () {
         this.length = 0;
--- tests/stack-spec.js
+++ tests/stack-spec.js
@@ -13,10 +13,10 @@ describe ('stack', function () {
         var stack = new Stack();
         expect(stack.push).toBeOfType('function');
 
-        expect(stack.push('value 1')).toBe('value 1');
+        expect(stack.push('item 1')).toBe('item 1');
         expect(stack.size()).toBe(1);
 
-        expect(stack.push('value 2')).toBe('value 2');
+        expect(stack.push('item 2')).toBe('item 2');
         expect(stack.size()).toBe(2);
     });
 
@@ -26,11 +26,11 @@ describe ('stack', function () {
         expect(stack.peek).toBeOfType('function');
         expect(stack.peek()).not.toBeDefined();
 
-        stack.push('value 1');
-        expect(stack.peek()).toBe('value 1');
+        stack.push('item 1');
+        expect(stack.peek()).toBe('item 1');
 
-        stack.push('value 2');
-        expect(stack.peek()).toBe('value 2');
+        stack.push('item 2');
+        expect(stack.peek()).toBe('item 2');
     });
 
     it ('can have its top data popped', function () {
@@ -39,14 +39,14 @@ describe ('stack', function () {
         expect(stack.pop).toBeOfType('function');
         expect(stack.pop()).not.toBeDefined(); // empty pop ok
 
-        stack.push('value 1');
-        stack.push('value 2');
+        stack.push('item 1');
+        stack.push('item 2');
         expect(stack.size()).toBe(2);
 
-        expect(stack.pop()).toBe('value 2');
+        expect(stack.pop()).toBe('item 2');
         expect(stack.size()).toBe(1);
 
-        expect(stack.pop()).toBe('value 1');
+        expect(stack.pop()).toBe('item 1');
         expect(stack.size()).toBe(0);
 
         expect(stack.pop()).not.toBeDefined();
@@ -57,12 +57,39 @@ describe ('stack', function () {
 
         expect(stack.clear).toBeOfType('function');
 
-        stack.push('value 1');
-        stack.push('value 2');
-        expect(stack.peek()).toBe('value 2');
+        stack.push('item 1');
+        stack.push('item 2');
+        expect(stack.peek()).toBe('item 2');
         expect(stack.size()).toBe(2);
 
         stack.clear();
         expect(stack.size()).toBe(0);
+    });
+
+    it ('can have a capacity set', function () {
+        var stack = new Stack();
+
+        expect(stack.capacity).toBeOfType('function');
+        expect(stack.capacity()).toBe(Infinity);
+        expect(stack.capacity(4)).toBe(4);
+
+        expect(stack.push('item 1')).toBe('item 1');
+        expect(stack.push('item 2')).toBe('item 2');
+        expect(stack.push('item 3')).toBe('item 3');
+        expect(stack.push('item 4')).toBe('item 4');
+        expect(stack.push('item 5')).not.toBeDefined();
+
+        expect(stack.peek()).toBe('item 4');
+        expect(stack.size()).toBe(4);
+
+        expect(stack.capacity(2)).toBe(2);
+        expect(stack.size()).toBe(2);
+        expect(stack.peek()).toBe('item 2');
+
+        expect(stack.capacity(Infinity)).toBe(Infinity);
+        expect(stack.push('item 3')).toBe('item 3');
+        expect(stack.push('item 4')).toBe('item 4');
+        expect(stack.push('item 5')).toBe('item 5');
+        expect(stack.push('item 6')).toBe('item 6');
     });
 });
