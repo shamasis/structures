330560ca226ebec2113bbf4b572dddedb7df8eb4
Shamasis Bhattacharya <shamasis@fusioncharts.com>
2014-06-11 19:14:17 +0530

    Minor whitespace lint error fix in jasmine spec file that stores helper matchers and added a file overview documentation

--- tests/matchers-spec.js
+++ tests/matchers-spec.js
@@ -1,6 +1,12 @@
+/**
+ * @fileOverview
+ *
+ * This file contains all helper jasmine matchers needed for generic unit testing of structures. This file should
+ * ideally not contain any non-matcher helper functions that may be needed in spec files.
+ */
 beforeEach(function() {
     this.addMatchers({
-        toBeOfType: function(type) {
+        toBeOfType: function (type) {
             return (typeof this.actual === type);
         }
     });
